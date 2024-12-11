0c0c52bb677a44e1728b231cb3d74ea1b9de6d3a
Artenlaclase <artenlaclase@gmail.com>
2024-12-11 09:38:43 -0300

    métodos  controller

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class GalleryController extends Controller
 {
-    //
+    public function cine()
+    {
+        // Retorna la vista para la página de "Cine Escolar"
+        return view('gallery.cine');
+    }
+
+    public function escultura()
+    {
+        // Retorna la vista para la página de "Escultura Geométrica"
+        return view('gallery.escultura');
+    }
+
+    public function trabajo()
+    {
+        // Retorna la vista para la página de "Trabajo en Equipo"
+        return view('gallery.trabajo');
+    }
 }
