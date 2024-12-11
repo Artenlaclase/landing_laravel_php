7c1d2b445d5fcd4d12ea5f99f90e13c73852abe0
Artenlaclase <artenlaclase@gmail.com>
2024-12-11 11:17:27 -0300

    pagina trabajo

---
+++ resources/views/gallery/trabajo.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<section id="trabajo" class="mb-5">
+    <h2 class="text-center my-5">Trabajo en Equipo</h2>
+    <div class="container">
+        <img src="/img/Trabajo_en_equipo.jpg" alt="Trabajo en equipo" class="img-fluid mb-4">
+        <p>
+            Actividades diseñadas para fomentar la colaboración entre estudiantes en diversos proyectos.
+        </p>
+    </div>
+</section>
+@endsection
