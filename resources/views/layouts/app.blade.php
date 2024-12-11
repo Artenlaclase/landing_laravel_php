117746bcfb0b42f35132103c4acd5c334ee848ad
Artenlaclase <artenlaclase@gmail.com>
2024-12-11 12:13:02 -0300

    stylo app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,19 +8,29 @@
 </head>
 <body>
     <header>
-        <nav>
-            <a href="{{ url('/') }}">Inicio</a>
-            <a href="{{ route('gallery.cine') }}">Cine Escolar</a>
-            <a href="{{ route('gallery.actividades') }}">Actividades Didácticas</a>
-            <a href="{{ route('gallery.trabajo') }}">Trabajo en Equipo</a>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="{{ url('/') }}">Inicio</a>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('gallery.cine') }}">Cine Escolar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('gallery.actividades') }}">Actividades Didácticas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('gallery.trabajo') }}">Trabajo en Equipo</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
     </header>
 
-    <main>
+    <main class="container mt-5">
         @yield('content')
     </main>
 
-    <footer>
+    <footer class="bg-dark text-white text-center py-3">
         <p>© 2024 Proyecto Artes Visuales</p>
     </footer>
 </body>
