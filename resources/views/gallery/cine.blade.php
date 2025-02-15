b2bea3f44f1de25b6942d7cd924346061344f87b
Artenlaclase <artenlaclase@gmail.com>
2025-02-15 19:43:15 -0300

    feat: texto e imagen orden

--- resources/views/gallery/cine.blade.php
+++ resources/views/gallery/cine.blade.php
@@ -4,10 +4,18 @@
 <section id="cine" class="mb-5">
     <h2 class="text-center my-5">Cine Escolar</h2>
     <div class="container">
-        <img src="/img/cine_escolar (1).jpg" alt="Cine escolar" class="img-fluid mb-4">
-        <p>
-            Este proyecto promueve la creatividad y el trabajo en equipo mediante la producción audiovisual.
-        </p>
+        <div class="row align-items-center flex-column flex-md-row">
+            <!-- Imagen a la izquierda -->
+            <div class="col-md-6 text-center text-md-start">
+                <img src="/img/cine_escolar (1).jpg" alt="Cine escolar" class="img-fluid rounded">
+            </div>
+            <!-- Texto a la derecha -->
+            <div class="col-md-6 text-start">
+                <p>
+                    Este proyecto promueve la creatividad y el trabajo en equipo mediante la producción audiovisual.
+                </p>
+            </div>
+        </div>
     </div>
 </section>
 @endsection
